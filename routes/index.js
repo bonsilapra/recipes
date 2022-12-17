632f59e819c9f2d4882bcbdf444e605f77a6a8be
bonsilapra <joanna.falkowska93@gmail.com>
2022-12-17 18:34:27 +0100

    Eslint fix; add `nodemon`

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const knexConnection = require('../db-connection.js')
-/* GET home page. */
+const knexConnection = require('../db-connection.js');
+
 router.get('/', function(req, res, next) {
-    const result = knexConnection('recipe')
-        .whereILike('name', `${req.query.query}%`)
-        .union(function() {
-            this.select('*')
-                .from('recipe')
-                .whereILike('name', `%${req.query.query}%`)
-            })
-        .then(
-            data => res.json(data)
-        )
-        .catch(
-            error => console.log('error', error)
-        )
-    
+	const result = knexConnection('recipe')
+		.whereILike('name', `${req.query.query}%`)
+		.union(function() {
+			this.select('*')
+				.from('recipe')
+				.whereILike('name', `%${req.query.query}%`);
+		})
+		.then(
+			data => res.json(data)
+		)
+		.catch(
+			error => console.log('error', error)
+		);
 });
 
 module.exports = router;
--- routes/ingredients.js
+++ routes/ingredients.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const knexConnection = require('../db-connection.js')
-/* GET home page. */
+const knexConnection = require('../db-connection.js');
+
 router.get('/', function(req, res, next) {
-    const result = knexConnection.select().from('ingredient')
-        .then(
-            data => res.json(data)
-        )
-        .catch(
-            error => console.log('error', error)
-        )
-    
+	const result = knexConnection.select().from('ingredient')
+		.then(
+			data => res.json(data)
+		)
+		.catch(
+			error => console.log('error', error)
+		);
 });
 
 module.exports = router;
