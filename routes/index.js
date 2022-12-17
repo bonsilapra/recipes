3b0f5b7195e0756bcb19166087ae8f716ec05c85
bonsilapra <joanna.falkowska93@gmail.com>
2022-12-17 17:45:04 +0100

    Fix typo; add database connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const knexConnection = require('../db-connection.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    const result = knexConnection.select('name', 'source').from('recipe')
+        .then(
+            data => res.json(data)
+        )
+        .catch(
+            error => console.log('error', error)
+        )
+    
 });
 
 module.exports = router;
