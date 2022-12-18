745afc9df3bf15dcc93f718d5dca6ec2ccddca07
bonsilapra <joanna.falkowska93@gmail.com>
2022-12-18 12:12:30 +0100

    Search recipe by text

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,37 @@ const router = express.Router();
 const knexConnection = require('../db-connection.js');
 
 router.get('/', function(req, res, next) {
-	const result = knexConnection('recipe')
-		.whereILike('name', `${req.query.query}%`)
-		.union(function() {
-			this.select('*')
-				.from('recipe')
-				.whereILike('name', `%${req.query.query}%`);
-		})
+	knexConnection('recipe').select().orderBy('name')
 		.then(
 			data => res.json(data)
 		)
 		.catch(
-			error => console.log('error', error)
+			error => {
+				console.log('error', error);
+				res.status(500).send('Something went wrong!');
+			}
+		);
+});
+
+router.get('/search', function(req, res, next) {
+	knexConnection('recipe')
+		.whereILike('name', `%${req.query.text}%`).orderBy('name')
+		.then(
+			data => {
+				data.sort((a, b) => {
+					const resultA = a.name.toLowerCase().indexOf(req.query.text);
+					const resultB = b.name.toLowerCase().indexOf(req.query.text);
+
+					return resultA - resultB;
+				});
+				res.json(data);
+			}
+		)
+		.catch(
+			error => {
+				console.log('error', error);
+				res.status(500).send('Something went wrong!');
+			}
 		);
 });
 
